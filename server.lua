242780c298f22bc96e6a640b182c6129d70111e0
포코 <107696090+poco8537@users.noreply.github.com>
2022-10-26 11:16:14 +0900

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,22 +5,21 @@ vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP", GetCurrentResourceName())
 
 local all_playeritemgive = {function(player,choice)
-    local playerlist = 0
+    local source = player
     vRP.prompt({source, '지급할 아이템 코드를 입력해주세요.', 'ex) poco_rewelcome', function(source, nameitem)
         if nameitem ~= nil or nameitem ~= "" then
             vRP.prompt({source, '지급할 아이템 개수를 입력하세요.', 'ex) 100', function(source, amounitem)
                 if amounitem ~= nil or amounitem ~= "" then
                     amounitem = tonumber(amounitem)
-                    if amounitem >= 1 then
-                        vRP.request({player, "허가 없는 아이템 지급은 해임 입니다.","정말로 아이템을 지급하시겠습니까", 30, function(player, ok)
+                    if amounitem > 1 then
+                        vRP.request({player, "모든 유저 아이템 지급","정말로 아이템을 지급하시겠습니까", 30, function(player, ok)
                             if ok then
                                 if vRP.getItemName({nameitem}) ~= nameitem then
                                     local users = vRP.getUsers({})
                                     for k,v in pairs(users) do
                                         local target_source = vRP.getUserSource({k})
                                         if target_source ~= nil then
-                                                vRP.giveInventoryItem({user_id, nameitem, amounitem})
-                                                playerlist = playerlist = 1
+                                            vRP.giveInventoryItem({user_id, nameitem, amounitem})
                                         end
                                     end
                                 else
@@ -29,7 +28,7 @@ local all_playeritemgive = {function(player,choice)
                             end
                         end})
                     else
-                        vRPclient.notify(source, {"아이템이 너무 적습니다. (최소 : 1개)"})
+                        vRPclient.notify(source, {"아이템이 너무 적습니다."})
                     end
                 else
                     vRPclient.notify(source, {"올바른 아이템 개수를 입력하세요."})
@@ -42,12 +41,12 @@ local all_playeritemgive = {function(player,choice)
 end,"모든 플레이어에게 아이템을 지급합니다."}
 
 
-vRP.registerMenuBuilder({"admin_general", function(add, data)
+vRP.registerMenuBuilder({"admin", function(add, data)
 	local user_id = vRP.getUserId({data.player})
 	if user_id ~= nil then
 		local choices = {}
 		if vRP.hasPermission({user_id, "all.playeritemgive"}) then
-		    choices["[12] 모든 유저 아이템 지급"] = all_playeritemgive
+		    choices["* 모든 유저 아이템 지급"] = all_playeritemgive
 		end
 		add(choices)
 	end
